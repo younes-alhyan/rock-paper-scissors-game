16033805e0fd2ea5f1a941257da047eac8f4b469
Yokai47 <gameryounes555@gmail.com>
2025-10-23 01:33:00 +0100

    style: adjust header height

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -4,9 +4,9 @@ interface HeaderProps {
 
 function Header({ score }: HeaderProps) {
   return (
-    <div className="w-[90vw] md:w-[80vw] lg:w-5xl flex items-center justify-between my-12 p-4 sm:px-8 border-4 border-gray-500 rounded-2xl z-10">
-      <img src="./logo.svg" alt="Logo" className="w-16 h-16 sm:w-32 sm:h-32" />
-      <div className="h-full flex flex-col items-center justify-center py-2 px-4 sm:px-12 bg-white rounded-md">
+    <div className="w-[90vw] md:w-[80vw] lg:w-5xl h-32 sm:h-42 flex items-stretch justify-between my-12 p-4 sm:px-8 border-4 border-gray-500 rounded-2xl z-10">
+      <img src="./logo.svg" alt="Logo"/>
+      <div className="flex flex-col items-center justify-center py-2 px-4 sm:px-12 bg-white rounded-md">
         <p className="text-blue-700 font-semibold tracking-wide">SCORE</p>
         <p className="text-gray-600 text-5xl sm:text-6xl">{score}</p>
       </div>
